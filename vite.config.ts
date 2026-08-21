24402e50d3b3e359d8e59f6ec51c5987726ed2c2
DariaVelvet <velvetdasha37@gmail.com>
2026-08-21 13:13:17 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ const emitSourcemaps = process.env.EMIT_SOURCEMAPS === "true";
 
 // Vite config — https://vitejs.dev/config/
 export default defineConfig({
-  base: '/anallizator/',
+  base: '/anallizator4.0/',
   
   build: {
     sourcemap: emitSourcemaps ? "inline" : false,
